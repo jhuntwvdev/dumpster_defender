29b174306ca229e48f811dd296224c6149253c28
Joshua Hunt <Joshua Hunt>
2025-09-05 12:49:40 -0400

    created rpg.go

---
+++ rpg.go
@@ -0,0 +1,13 @@
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Dare ye enter?")
+	var options int = 5
+	lobbyRoom(options)
+}
+
+func lobbyRoom (options) {
+	fmt.Println("You enter a creepy lobby")
+}
